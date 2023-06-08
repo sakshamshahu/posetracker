c7b1f9a0c924590b9cda53ecaf23e06531510a27
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-06-08 12:42:37 +0530

    feat: joint detection

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ mp_pose = mp.solutions.pose
 # Video feed
 cap = cv.VideoCapture(0)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv.imshow('Mediapipe Feed', frame)
-    if cv.waitKey(10) & 0xFF == ord('x'):
-        break
+#setting up the mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-cap.release()
-cv.destroyAllWindows()
+        #Recoloring to RGB to pass img to mediapipe
+        image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        image.flags.writeable = False #image is no longer writeable
+
+        results = pose.process(image)
+
+        #Recoloring back to BGR
+        image.flags.writeable = True
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+
+        #Rendering detections onto the feed
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        cv.imshow('Video Feed', image)
+        if cv.waitKey(1) & 0xFF == ord('x'):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
 
