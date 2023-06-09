48c6150366b15e42cd599f9ef469a81074f436ad
Saksham-Shahu <sakshamshahu.work@gmail.com>
2023-06-09 13:50:15 +0530

    refactor: angleCalc

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-from angle import angle
+from angleCalc import angle
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
